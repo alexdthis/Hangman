477556a6f188f65db1c4f237a3d3ed52d73cb707
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-30 15:03:46 -0700

    Finish basic functionality for hangman game
    Finished up the game class and word generator class.
    Most variables have been checked and corrected.
    Game should have basic playability now.
    Only issue is that the wrong choice counter goes down even
    when you type in save.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -9,13 +9,13 @@ class Word_generator
         line_count = 1
         File.open ('google-10000-english-no-swears.txt') do |file|
             file.each do |line|
-                if line_count == random_word
+                if line_count == random_number
                     chosen_word = line
                 end
                 line_count += 1
             end
         end
-        chosen_word.split()
+        chosen_word.strip.split('')
     end
     
     def fetch_word
@@ -26,22 +26,23 @@ end
 
 class Game
 
-#Starts a new Game class with either a new game option or load game option
+#Starts a new Game with either a new game option or load game option
 
     def initialize(loaded_state)
         @load_state = loaded_state
     end
 
-#if a new game is started, starts with all conditions initialized
-#if a game is loaded, starts with conditions from the specified text file
+#check a new game is started, starts with all conditions initialized
+#check a game is loaded, starts with conditions from the specified text file
 
     def new_or_load
         case @load_state
         when 1
-            @turns = 11
+            @turns = 0
             @history = Array.new(0)
             @chosen_word = Word_generator.new.fetch_word
             @mystery = self.puzzle_gen
+            @wrong_choices = 0
         when 2
             #present user with list of saves
             self.list_saves
@@ -49,9 +50,11 @@ class Game
             self.choose_save
             #assign variables from the file then put them in the game loop
             load_game
-            @turns = @save_state[0]
-            @chosen_word = @save_state[1]
-            @history = @save_state[2]
+            @turns = @save_state[0].to_i
+            @chosen_word = @save_state[1].split('')
+            @history = @save_state[2].split('')
+            @wrong_choices = @save_state[3].to_i
+            @mystery = self.puzzle_gen
         end
     end
 
@@ -76,15 +79,16 @@ class Game
 #Function to save the game when the phrase is typed, Generates a text file containing the index number of the word
 #in the 10000 words text file along with turns remaining, played characters, and current representation of the underlines array
 
-    def save_game(turns, random_word, history)
+    def save_game
         file_number = 1
         while File.exist?('Save#{file_number}.txt')
             file_number += 1
         end
         File.open("./saves/Save#{file_number}.txt", 'w') do |file|
             file.puts @turns
-            file.puts @random_word
-            file.puts @history
+            file.puts @chosen_word.join('')
+            file.puts @history.join(' ')
+            file.puts @wrong_choices.to_i
             file.puts "Created: #{Time.now}"
         end
     end
@@ -110,8 +114,9 @@ class Game
         @save_state
     end
 
+#function that takes in input, either as a single character or as 'save'
+
     def input_character
-        @history = Array.new(0)
         @input = ''
         #Sanitize the input and check it either more than one character, special characters, or the special save phrase
         until (@input =~ /[a-zA-Z]/ && @input.size == 1) || (@input == 'save')
@@ -120,6 +125,9 @@ class Game
         end
         if @input == 'save'
             self.save_game
+            @input = ' '
+        else
+            @turns += 1
         end
     end
 
@@ -129,68 +137,102 @@ class Game
 
     def word_checker
         @history.append(@input)
+        word_is_wrong = true
         @chosen_word.each_index do |index|
             if @chosen_word[index] == @input
                 @mystery[index] = @input
+                word_is_wrong = false
             end
+        end
+        if @input == 'save'
+            word_is_wrong = false
+        end
+        if word_is_wrong
+            @wrong_choices += 1
         end
     end 
 
 #draws a visual of a hanged man, line by line
 
-    def draw_hanged_man(turns)
-        puts "Turns left: #{@turns}"
-        if @turns <= 10
+    def draw_hanged_man
+        
+        if @wrong_choices >= 1
             puts "   ___   "
         end
-        if @turns <= 9
+        if @wrong_choices >= 2
             puts "  /. .\\  "
         end
-        if @turns <= 8
+        if @wrong_choices >= 3
             puts "  | ~ |  "
         end
-        if @turns <= 7
+        if @wrong_choices >= 4
             puts "  \\___/  "
         end
-        if @turns <= 6
+        if @wrong_choices >= 5
             puts "    |    "
         end
-        if @turns <= 5
+        if @wrong_choices >= 6
             puts "   /|\\   "
         end
-        if @turns <= 4
+        if @wrong_choices >= 7
             puts "  / | \\  "
         end
-        if @turns <= 3
+        if @wrong_choices >= 8
             puts "    |    "
         end
-        if @turns <= 2
+        if @wrong_choices >= 9
             puts "    |    "
         end
-        if @turns <= 1
+        if @wrong_choices >= 10
             puts "   / \\   "
         end
-        if @turns == 0
+        if @wrong_choices >= 11
             puts "  /   \\  "
         end
     end
 
-#actual game function loop, this will be the source of the function calls in this class
+ #checks the word has been guessed correctly
+
+    def check_for_win
+        if @mystery.none? {|character| character == '_'}
+            @game_won = true
+        end
+    end
+
+ #actual game function loop, this will be the source of the function calls
 
     def game
+        @game_won = false
         self.new_or_load
-        while @turns != 0
+        until ((@wrong_choices == 11) || @game_won)
+            system 'clear'
+            self.draw_hanged_man
+            puts "Turn: #{@turns}"
+            puts "Choices left until this man is hanged: #{12 - @wrong_choices}"
+            puts "Word: #{@mystery.join(' ')}"
+            puts "Characters played: #{@history.join(' ')}"
             self.input_character
             self.word_checker
-            self.draw_hanged_man
+            
+            self.check_for_win
+        end
+        if @game_won
+            puts "Your winner!!!!"
+            puts "The answer was #{@chosen_word.join('')}"
+        else
+            puts "YOU LOSSSSS!!!!"
+            puts "The answer was #{@chosen_word.join('')}"
         end
     end
 end
 
 #game function loop
 
+
 input = ''
-until ['1','2'].include?(input)
+until [1,2].include?(input)
     puts "Type 1 to start a new game or 2 to load a save"
-    input = gets.chomp
+    input = gets.chomp.to_i
 end
+
+Game.new(input.to_i).game
