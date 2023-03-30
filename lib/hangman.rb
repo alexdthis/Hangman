2bfdf4c5a02370da9c528b6b2bfa55b041da6574
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-29 20:00:39 -0700

    Type in comments that will be filled in with code

---
+++ lib/hangman.rb
@@ -0,0 +1,25 @@
+#open the text file
+
+#Random number generator
+
+#Select the word from the file
+
+#Store the word in an array and generate another string containing length of word underlines
+
+#Prompt the user for a character
+
+#Sanitize the input and check if it either more than one character, special characters, or the special save phrase
+
+#Check the inputted character against the chosen word array
+
+#Store the character inside another array to represent characters already played
+
+#If the character is in the mystery word, go through the underlines array and replace all corresponding indexes with the character
+
+#Increment by one turn/Draw one line on the hanged man
+
+#Function to save the game when the phrase is typed, Generates a text file containing the index number of the word
+#in the 10000 words text file along with turns remaining, played characters, and current representation of the underlines array
+
+#Function to draw the hanged man
+
