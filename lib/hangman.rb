9a95817d8a55a021bd52c7d1f0718136798703fd
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-03-30 13:12:01 -0700

    Create separate game classes, add save function
    Segregated the game function and the word generator
    into two classes. Also added save functionality by saving
    text files into a folder in the working directory.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -7,8 +7,6 @@ class Word_generator
     def random_word(random_number)
         chosen_word = ''
         line_count = 1
-
-        #open the text file
         File.open ('google-10000-english-no-swears.txt') do |file|
             file.each do |line|
                 if line_count == random_word
@@ -17,83 +15,61 @@ class Word_generator
                 line_count += 1
             end
         end
-        chosen_word
+        chosen_word.split()
     end
     
-    #Store the word in an array and generate another string containing length of word underlines
-    def word_processor
-        chosen_word = chosen_word.split('')
-        length_of_puzzle = chosen_word.size
-        mystery = Array.new(length_of_puzzle, '_')
-    end
-
     def fetch_word
         random_number = self.random_number()
         random_word = self.random_word(random_number)
-        processed_word = self.word_processor(random_word)
     end
-end
-#Increment by one turn/Draw one line on the hanged man
-
-turns -= 1
-#draw hanged_man(turns)
-
-
-        
-
-#game function loop
-
-input = ''
-until ['1','2'].include?(input)
-    puts "Type 1 to start a new game or 2 to load a save"
-    input = gets.chomp
-end
-
-case input
-when '1'
-    Game.new(1)
-when '2'
-    Game.new(2)
 end
 
 class Game
 
-    
+#Starts a new Game class with either a new game option or load game option
+
     def initialize(loaded_state)
         @load_state = loaded_state
     end
+
+#if a new game is started, starts with all conditions initialized
+#if a game is loaded, starts with conditions from the specified text file
 
     def new_or_load
         case @load_state
         when 1
             @turns = 11
-            #new game
+            @history = Array.new(0)
+            @chosen_word = Word_generator.new.fetch_word
+            @mystery = self.puzzle_gen
         when 2
             #present user with list of saves
+            self.list_saves
             #prompt them to type in a number
+            self.choose_save
             #assign variables from the file then put them in the game loop
+            load_game
+            @turns = @save_state[0]
+            @chosen_word = @save_state[1]
+            @history = @save_state[2]
         end
     end
 
-    def game
-        self.new_or_load
-        while @turns != 0
-            input_character
-            word_checker
-            draw_hanged_man
-        end
+#generates a string containing only '_' of the same length as the chosen mystery word
+
+    def puzzle_gen
+        length_of_puzzle = @chosen_word.size
+        Array.new(length_of_puzzle, '_')
     end
 
-    def input_character
-        @history = Array.new(0)
-        #Sanitize the input and check it either more than one character, special characters, or the special save phrase
-        until (@input =~ /[a-zA-Z] && @input.size == 1) || (@input == 'save')
-            puts "Enter a character or type in save to save your progress"
-            @input = gets.strip.downcase.chomp
-        end
-        if @input == 'save'
-            #save function
-            save_game(turns, random_word, history)
+#lists all saves in the saves directory
+
+    def list_saves
+        save_list = Dir.entries('./saves/')
+        save_list.each do |file|
+            if file.include?('Save')
+                puts file
+            end
         end
     end
 
@@ -113,36 +89,54 @@ class Game
         end
     end
 
-    #function to load the game
+#lets the user input a number to choose a save listed in the directory
 
-    def load_game(choice)
-        save_state = Array.new(0)
-        File.open("./saves/Save#{choice}.txt", 'r') do |file|
-            file.each do |line|
-                save_state.append(line)
-            end
+    def choose_save
+        @choice = ''
+        until @choice =~ /[0-9]/ do
+            @choice = gets.chomp
         end
-        save_state
     end
 
-    #Check the inputted character against the chosen word array
-    #Store the character inside another array to represent characters already played
-    #If the character is in the mystery word, go through the underlines array and replace all corresponding indexes with the character
+#function to load the game
+
+    def load_game
+        @save_state = Array.new(0)
+        File.open("./saves/Save#{@choice}.txt", 'r') do |file|
+            file.each do |line|
+                @save_state.append(line)
+            end
+        end
+        @save_state
+    end
+
+    def input_character
+        @history = Array.new(0)
+        @input = ''
+        #Sanitize the input and check it either more than one character, special characters, or the special save phrase
+        until (@input =~ /[a-zA-Z]/ && @input.size == 1) || (@input == 'save')
+            puts "Enter a character or type in save to save your progress"
+            @input = gets.strip.downcase.chomp
+        end
+        if @input == 'save'
+            self.save_game
+        end
+    end
+
+#Check the inputted character against the chosen word array
+#Store the character inside another array to represent characters already played
+#If the character is in the mystery word, go through the underlines array and replace all corresponding indexes with the character
+
     def word_checker
-        @history.append(input)
-        chosen_word.each_index do |index|
-            if chosen_word[index] == @input
+        @history.append(@input)
+        @chosen_word.each_index do |index|
+            if @chosen_word[index] == @input
                 @mystery[index] = @input
             end
         end
     end 
 
-    #function to assign variables when game is loaded
-    def assign_var_on_load
-        @turns = save_state[0]
-        @random_word = save_state[1]
-        @history = save_state[2]
-    end
+#draws a visual of a hanged man, line by line
 
     def draw_hanged_man(turns)
         puts "Turns left: #{@turns}"
@@ -180,4 +174,23 @@ class Game
             puts "  /   \\  "
         end
     end
+
+#actual game function loop, this will be the source of the function calls in this class
+
+    def game
+        self.new_or_load
+        while @turns != 0
+            self.input_character
+            self.word_checker
+            self.draw_hanged_man
+        end
+    end
+end
+
+#game function loop
+
+input = ''
+until ['1','2'].include?(input)
+    puts "Type 1 to start a new game or 2 to load a save"
+    input = gets.chomp
 end
